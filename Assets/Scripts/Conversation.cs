0a4b23cbd4f47657f5210a84854f1dbec9e78a72
Alberto Ramos <aramos@egogames.com>
2020-08-14 22:00:13 +0200

    Fixed bug that made only one word fade in/out

--- Assets/Scripts/Conversation.cs
+++ Assets/Scripts/Conversation.cs
@@ -17,20 +17,19 @@ public class Conversation : MonoBehaviour
     public float m_wordScreenDistanceOffset;
     public float m_wordDistanceOffset;
 
-
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(DisplayLine(Character.Computer, 0));
     }
 
-    IEnumerator DisplayLine(Character l_char, int l_index, float l_time = 1.5f)
+    private IEnumerator DisplayLine(Character l_char, int l_index, float l_time = 1)
     {
         //if (m_computerText.text != "")
         //{
         //    yield return StartCoroutine(FadeText(false, m_computerText, l_time));
         //}
-        yield return StartCoroutine(SpawnWordPopups("ola ke ase"));
+        yield return StartCoroutine(SpawnWordPopups("^^ :3 jaja"));
         if (l_char == Character.Computer)
         {
             m_computerText.text = m_computerLines[l_index];
@@ -41,7 +40,6 @@ public class Conversation : MonoBehaviour
         {
             m_playerText.text = m_playerLines[l_index];
             StartCoroutine(FadeText(true, m_playerText, l_time));
-
         }
     }
 
@@ -49,7 +47,7 @@ public class Conversation : MonoBehaviour
     {
         foreach (var loc in l_list)
         {
-            if (Mathf.Abs(l_location.x - loc.x ) > m_wordDistanceOffset || Mathf.Abs(l_location.y - loc.y) > m_wordDistanceOffset)
+            if (Mathf.Abs(l_location.x - loc.x) < m_wordDistanceOffset || Mathf.Abs(l_location.y - loc.y) < m_wordDistanceOffset)
             {
                 return true;
             }
@@ -57,51 +55,44 @@ public class Conversation : MonoBehaviour
         return false;
     }
 
-    IEnumerator SpawnWordPopups(string l_line)
+    private IEnumerator SpawnWordPopups(string l_line)
     {
         string[] words = l_line.Split(' ');
         List<Vector3> wordPositions = new List<Vector3>(); 
         for (int i = 0; i < words.Length; ++i)
         {
             Vector3 spawnLocation = new Vector3(Random.Range(m_wordScreenDistanceOffset, Screen.width - m_wordScreenDistanceOffset), Random.Range(m_wordScreenDistanceOffset, Screen.height - m_wordScreenDistanceOffset), 0);
-            //if (IsWordOverlapping(wordPositions, spawnLocation))
-            //{
-            //    Debug.Log(i);
-            //    continue;
-            //}
-            //wordPositions.Add(spawnLocation);
-            Debug.Log(wordPositions.Count);
+            if (IsWordOverlapping(wordPositions, spawnLocation))
+            {
+                --i;
+                continue;
+            }
+            wordPositions.Add(spawnLocation);
             GameObject wordContainer = Instantiate(m_wordContainer, transform.parent);
             wordContainer.transform.position = spawnLocation;
             TextMeshProUGUI tmpText = wordContainer.GetComponentInChildren<TextMeshProUGUI>();
             tmpText.text = words[i];
-
-            yield return FadeText(true, tmpText, 2);
-
-            yield return new WaitForSeconds(m_wordSpawnTime);
+            
+            yield return FadeText(true, tmpText, 1.5f);
+            
         }
     }
 
-    IEnumerator FadeText(bool l_fadeIn, TextMeshProUGUI l_textBox, float l_time)
+    private IEnumerator FadeText(bool l_fadeIn, TextMeshProUGUI l_textBox, float l_duration)
     {
-        float time = Time.time;
-        float until = Time.time + l_time;
+        float start = Time.time;
+        float until = start + l_duration;
         int alphaFrom = l_fadeIn ? 0 : 1;
-        int alphaGoal = l_fadeIn ? 1 : 0;
+        int alphaGoal = 1 - alphaFrom;
         Color textColor = l_textBox.color;
-        textColor.a = l_fadeIn ? 0 : 1;
-        l_textBox.color = textColor;
 
-        while (time < until)
+        while (Time.time < until)
         {
-            textColor = l_textBox.color;
-            textColor.a = Mathf.Lerp(alphaFrom, alphaGoal, Time.time / l_time);
+            textColor.a = Mathf.Lerp(alphaFrom, alphaGoal, (Time.time - start) / l_duration);
             l_textBox.color = textColor;
-            time += Time.deltaTime;
             yield return null;
         }
-        textColor.a = l_fadeIn ? 1 : 0;
+        textColor.a = alphaGoal;
         l_textBox.color = textColor;
-
     }
 }
